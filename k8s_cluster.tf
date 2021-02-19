9893d32ae9d0950c8ce4b2ce6a9fa7ea35c9e3bb
Gyllenhammar <Gyllenhammar@users.noreply.github.com>
2021-02-19 12:37:13 +0100

    k8s node creation

--- k8s_cluster.tf
+++ k8s_cluster.tf
@@ -1,13 +1,14 @@
 #k8s cluster
 
-### k8s_1
+### k8s nodes
 
-resource "azurerm_linux_virtual_machine" "k8s_1" {
-    name                  = var.vm_1-name
-    location              = var.location
-    resource_group_name   = azurerm_resource_group.main.name
-    network_interface_ids = [azurerm_network_interface.k8s_1-nic.id]
-    size                  = "Standard_DS1_v2"
+resource "azurerm_linux_virtual_machine" "node" {
+    count                   = var.vm_count
+    name                    = "${var.prefix}-${var.vm_name}-${count.index}"
+    location                = var.location
+    resource_group_name     = azurerm_resource_group.main.name
+    network_interface_ids   = [azurerm_network_interface.node_nic[count.index].id]
+    size                    = var.vm_size
 
     os_disk {
         caching           = "ReadWrite"
@@ -21,62 +22,97 @@ resource "azurerm_linux_virtual_machine" "k8s_1" {
         version   = "latest"
     }
 
-    computer_name  = var.vm_1-name
-    admin_username = var.linuxAdminUsername
+    computer_name  = "${var.vm_name}-${count.index}"
+    admin_username = var.vm_admin
     disable_password_authentication = true
 
-    custom_data = data.template_cloudinit_config.config.rendered
+    #custom_data = data.cloudinit_config.config.rendered
+    custom_data = base64encode(data.template_file.cloudconfig.rendered)
 
     admin_ssh_key {
-        username       = var.linuxAdminUsername
+        username       = var.vm_admin
         public_key     = file("~/.ssh/id_rsa.pub")
     }
 
     tags = {
+        name = "Server ${var.vm_name}-${count.index}"
+        purpose     = var.purpose
         environment = var.environment
+        owner       = var.owner
+        group       = var.group
+        costcenter  = var.costcenter
+        application = var.application
     }
 }
 
 #Cloud-init
-data "template_cloudinit_config" "config" {
-  gzip          = true
-  base64_encode = true
+# data "cloudinit_config" "config" {
+#   gzip          = true
+#   base64_encode = true
 
-  # Main cloud-config configuration file.
-  part {
-    content_type = "text/cloud-config"
-    content      = "packages: ['snap']"
+#   vars = {
+#       username = var.vm_admin
+#   }
+
+#   # Main cloud-config configuration file.
+#   part {
+#     content_type = "text/cloud-config"
+#     content      = file("${path.module}/scripts/node_cloudinit.yaml")
+#   }
+# }
+
+data "template_file" "cloudconfig" {
+    template = file("${path.module}/scripts/node_cloudinit.yaml")
+
+    vars = {
+      username = var.vm_admin
   }
-
-  part {
-    content_type    = "text/x-shellscript"
-    content         = "sudo snap install microk8s"     
-  } 
 }
 
-resource "azurerm_public_ip" "k8s_1-pub-ip" {
-    name = "k8s_1-pub-ip"
-    location = var.location
+resource "azurerm_public_ip" "node_public_ip" {
+    count               = var.vm_count
+    name                = "${var.prefix}-node_public_ip-${count.index}"
+    location            = var.location
     resource_group_name = azurerm_resource_group.main.name
-    allocation_method = "Dynamic"
+    allocation_method   = "Dynamic"
+
+    tags = {
+        purpose     = var.purpose
+        environment = var.environment
+        owner       = var.owner
+        group       = var.group
+        costcenter  = var.costcenter
+        application = var.application
+    }
 }
 
-resource "azurerm_network_interface" "k8s_1-nic" {
-    name = "k8s_1-nic"
-    location = var.location
+resource "azurerm_network_interface" "node_nic" {
+    count               = var.vm_count
+    name                = "${var.prefix}-node_nic-${count.index}"
+    location            = var.location
     resource_group_name = azurerm_resource_group.main.name
 
     ip_configuration {
-        name = "k8s_1-nic-conf"
-        subnet_id = azurerm_subnet.subnet_01.id
+        name = "node_nic_conf-${count.index}"
+        subnet_id = azurerm_subnet.subnet.id
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id = azurerm_public_ip.k8s_1-pub-ip.id
+        public_ip_address_id = azurerm_public_ip.node_public_ip[count.index].id
 
+    }
+
+    tags = {
+        purpose     = var.purpose
+        environment = var.environment
+        owner       = var.owner
+        group       = var.group
+        costcenter  = var.costcenter
+        application = var.application
     }
 
 }
 
-resource "azurerm_network_interface_security_group_association" "k8s_1-sga" {
-    network_interface_id = azurerm_network_interface.k8s_1-nic.id
-    network_security_group_id = azurerm_network_security_group.subnet_01.id
+resource "azurerm_network_interface_security_group_association" "node_sga" {
+    count = var.vm_count
+    network_interface_id = azurerm_network_interface.node_nic[count.index].id
+    network_security_group_id = azurerm_network_security_group.subnet.id
 }

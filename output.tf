cfc1a02027645a680164460260c5e3da8d300eaa
Gyllenhammar <Gyllenhammar@users.noreply.github.com>
2021-02-19 13:03:48 +0100

    added ssh output

--- output.tf
+++ output.tf
@@ -5,10 +5,14 @@ data "azurerm_public_ip" "public_ip" {
   depends_on = [ azurerm_public_ip.node_public_ip, azurerm_linux_virtual_machine.node ]
 }
 
-output "instance-pub-ip" {
+output "node_public_ips" {
     value = formatlist("%s: %s", azurerm_linux_virtual_machine.node.*.name, data.azurerm_public_ip.public_ip.*.ip_address) 
 }
 
-output "test" {
+output "node_ssh_connect" {
+    value = formatlist("%s: ssh %s@%s", azurerm_linux_virtual_machine.node.*.tags.name, var.vm_admin, data.azurerm_public_ip.public_ip.*.ip_address) 
+}
+
+output "cloudinit_config_file" {
     value = data.template_file.cloudconfig.rendered
 }

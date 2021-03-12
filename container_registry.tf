f40206989699aeae9808c2c5e7bb64811a658842
Gyllenhammar <Gyllenhammar@users.noreply.github.com>
2021-03-12 11:47:57 +0100

    removed container reg

--- container_registry.tf
+++
@@ -1,9 +0,0 @@
-# Container Registry
-
-# resource "azurerm_container_registry" "acr" {
-#     name                = lower(replace("${var.prefix}-ContainerRegistry", "-", ""))
-#     resource_group_name = azurerm_resource_group.main.name
-#     location            = var.location
-#     sku                 = "Basic"
-#     admin_enabled       = true
-# }
